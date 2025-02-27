226eb500f68ac8888cd9eccf81ebce87c757a857
edogola4 <edogola4@gmail.com>
2025-02-27 11:06:29 +0300

    defined relatinship in clothing

--- backend/app/Models/Clothing.php
+++ backend/app/Models/Clothing.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Clothing extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'category_id',
+        'user_id',
+        'color',
+        'size',
+        'brand',
+        'image_path',
+        'description'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
