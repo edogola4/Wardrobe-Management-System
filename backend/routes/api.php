2f4aab951a769cbed80129b8bd4f1b7673d2a4fd
edogola4 <edogola4@gmail.com>
2025-02-28 12:05:59 +0300

    updated

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ClothingController;
@@ -24,4 +24,22 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::fallback(function(){
         return response()->json(['message' => 'Not Found.'], 404);
     });
+});
+*/
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\ClothingController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('/clothing', [ClothingController::class, 'index']);
+    Route::post('/clothing', [ClothingController::class, 'store']);
+    Route::get('/clothing/{id}', [ClothingController::class, 'show']);
+    Route::put('/clothing/{id}', [ClothingController::class, 'update']);
+    Route::delete('/clothing/{id}', [ClothingController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
