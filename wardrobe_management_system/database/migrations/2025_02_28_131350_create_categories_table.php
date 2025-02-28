18275ced8d8863c2a23efb904a555cf549133408
edogola4 <edogola4@gmail.com>
2025-02-28 16:21:50 +0300

    updated

--- wardrobe_management_system/database/migrations/2025_02_28_131350_create_categories_table.php
+++ wardrobe_management_system/database/migrations/2025_02_28_131350_create_categories_table.php
@@ -1,27 +1,23 @@
 <?php
 
+//use Illuminate\Database\Migration\Migration;
+//use Illuminate\Database\Schema\BluePrint;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateCategoriesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('categories', function (BluePrint $table) {
             $table->id();
+            $table->string('name')->unique();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('categories');
     }
-};
+}
--- wardrobe_management_system/database/migrations/2025_02_28_131402_create_clothing_items_table.php
+++ wardrobe_management_system/database/migrations/2025_02_28_131402_create_clothing_items_table.php
@@ -1,27 +1,26 @@
 <?php
 
+//use Illuminate\Database\Migration\Migration;
+//use Illuminate\Database\Schema\BluePrint;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateClothingItemsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
-        Schema::create('clothing_items', function (Blueprint $table) {
+        Schema::create('clothing_items', function (BluePrint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('category_id')->constrained();
+            $table->string('name');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('clothing_items');
     }
-};
+}
