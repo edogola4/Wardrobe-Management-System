d585027b3bff6eb615adeaba02110a02c51fc421
edogola4 <edogola4@gmail.com>
2025-03-03 11:33:15 +0300

    updated

--- my-app/src/main.js
+++ my-app/src/main.js
@@ -1,5 +1,31 @@
-import { createApp } from 'vue'
+/*import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+*/
+
+
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
+import router from './routes'
+import Axios from 'axios'
+
+loadFonts()
+
+const app = createApp(App)
+app.use(vuetify)
+app.use(router)
+app.mount('#app')
+
+Axios.defaults.baseURL = 'http://localhost:8000'
+Axios.interceptors.request.use(config => {
+    const token = localStorage.getItem('access_token')
+    if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`
+    }
+    return config
+})
